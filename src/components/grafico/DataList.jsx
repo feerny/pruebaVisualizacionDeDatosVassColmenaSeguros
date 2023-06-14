11fb87dc23bed494b18ab6a03b224b40e6d5c127
feerny <luisfpe19@gmail.com>
2023-06-14 09:31:38 -0500

    termine de comentar codigo

--- src/components/grafico/DataList.jsx
+++ src/components/grafico/DataList.jsx
@@ -1,10 +1,11 @@
+//importaciones
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { FormControl, IconButton, MenuItem, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material';
 import { styled } from '@mui/system';
 import { Edit as EditIcon, Save as SaveIcon } from '@mui/icons-material';
 import { setDataList,setOptionData } from '../../redux/actions';
-
+//creo mis contenedores y les doy estilos
 const RootContainer = styled('div')({
   margin: '16px',
 });
@@ -15,9 +16,11 @@ const SelectContainer = styled(FormControl)({
 });
 
 function DataList({ dataList, setDataList, setOptionData,optionData }) {
+  // inicializo el estado editableData como un objeto vacío.
   const [editableData, setEditableData] = useState({});
+  //se inicializa como null. Esta variable se utilizará para almacenar los datos que se mostrarán según la opción seleccionada 
   let dataToShow = null;
-
+  //se asigna el valor correspondiente a dataToShow según la opción seleccionada en optionData
   if (optionData === 'ventasPorMes') {
     dataToShow = dataList.ventasPorMes;
   } else if (optionData === 'usuariosRegistradosPorMes') {
@@ -25,32 +28,29 @@ function DataList({ dataList, setDataList, setOptionData,optionData }) {
   } else if (optionData === 'ventasPorRegion') {
     dataToShow = dataList.ventasPorRegion;
   }
-
+  //funcion para manejar el cambio de opcion de data
   const handleOptionChange = (event) => {
     event.preventDefault();
+    //envia el valor de la opcion seleccionada al action
     setOptionData(event.target.value);
   };
 
   const handleInputChange = (key, value) => {
     // Actualizar el valor en el estado mientras se edita
-    setEditableData((prevState) => ({
-      ...prevState,
-      [key]: value,
-    }));
+    setEditableData({[key]: value});
   };
 
   const handleEdit = (key) => {
     // Establecer el valor en el estado editableData solo cuando se hace clic en el botón de editar
-    setEditableData((prevState) => ({
-      ...prevState,
-      [key]: dataToShow[key] || '',
-    }));
+    setEditableData({
+      [key]: dataToShow[key]
+    });
   };
 
   const handleSave = (key) => {
     const updatedValue = editableData[key];
 
-    // Actualizar el valor en el objeto de datos
+    // Actualizar solo el valor necesario en el objeto de datos
     const updatedDataList = {
       ...dataList,
       [optionData]: {
@@ -91,6 +91,7 @@ function DataList({ dataList, setDataList, setOptionData,optionData }) {
             </TableHead>
             <TableBody>
               {Object.entries(dataToShow).map(([key, value]) => {
+                //determina si editableData tiene la propiedad key usando hasOwnProperty
                 const isEditing = editableData.hasOwnProperty(key);
 
                 return (
@@ -127,17 +128,17 @@ function DataList({ dataList, setDataList, setOptionData,optionData }) {
     </RootContainer>
   );
 }
-
+//toma el estado actual del Redux store como parámetro y devuelve un objeto que mapea las partes relevantes del estado a props
 const mapStateToProps = (state) => {
   return {
     dataList: state.dataList,
     optionData:state.optionData
   };
 };
-
+//objeto que especifica qué acciones se deben vincular al componente
 const mapDispatchToProps = {
   setDataList,
   setOptionData
 };
-
+//conecta el componente DataList con el Redux store
 export default connect(mapStateToProps, mapDispatchToProps)(DataList);
